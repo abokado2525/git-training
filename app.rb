c067a786121951e992402b8c95b17c7763a31562
hayashida <hayashida.yukihisa146@mail.kyutech.jp>
2022-08-13 13:26:25 +0900

    /hayashidaエンドポイントを追加した

--- app.rb
+++ app.rb
@@ -5,12 +5,7 @@ get '/' do
   'Hello world!'
 end
 
-<<<<<<< HEAD
 
 get '/hayashida' do
   'Hello hayashida!'
-=======
-get '/abokado' do
-  'Hello abokado!!'
->>>>>>> 26667814653b7c1f28cc9319312c987ecc8018eb
 end
